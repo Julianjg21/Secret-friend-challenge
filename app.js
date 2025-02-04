9f5acb80bdb83512e64f4791bf05d5a481250b87
Julian Jimenez <julianjimenez2128@gmail.com>
2025-02-04 18:43:55 -0500

    feat: add functions to order friends list and pick a random friend in app.js

--- app.js
+++ app.js
@@ -17,3 +17,25 @@ const addFriend = () => {
   orderFriendsList();
 };
 
+const orderFriendsList = () => {
+  //Obtain the input element for your ID
+  const friendsListUl = document.getElementById("friendsList");
+  //Clean the first element
+  friendsListUl.innerHTML = "";
+  //Resort array friends
+  for (let friend in friends) {
+    //Add friend on the list with your html
+    friendsListUl.innerHTML += `<li>${friends[friend]}</li>`;
+  }
+};
+
+const pickRandomFriend = () => {
+  //Validate that the array is not empty
+  if (friends.length === 0) return;
+  //Create random number
+  const pickFriend = Math.floor(Math.random() * friends.length);
+  //Insert HTML
+  document.getElementById(
+    "result"
+  ).innerHTML = `<li>${friends[pickFriend]}</li>`;
+};
