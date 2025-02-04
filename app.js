8fed4895165337c6fde481b867a1af203453212b
Julian Jimenez <julianjimenez2128@gmail.com>
2025-02-04 18:43:33 -0500

    feat: add function to append friends to the list in app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+const addFriend = () => {
+  //Obtain the input element for your ID
+  const addNameInput = document.getElementById("friend");
+  //Obtain the value of the input
+  const valor = addNameInput.value;
+  if (valor === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+  //push the value to Array
+  friends.push(valor);
+  //Restore the input text
+  addNameInput.value = "";
+  //Add in friend on list
+  orderFriendsList();
+};
+
