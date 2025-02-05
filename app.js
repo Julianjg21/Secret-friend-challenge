d9d72bef7ef2cbe7f82bd807cc7c24e6fe4b6a31
Julian Jimenez <julianjimenez2128@gmail.com>
2025-02-04 20:03:15 -0500

    feat: implement dynamic background color change and update friend display in app.js; update button action in index.html

--- app.js
+++ app.js
@@ -37,5 +37,38 @@ const pickRandomFriend = () => {
   //Insert HTML
   document.getElementById(
     "result"
-  ).innerHTML = `<li>${friends[pickFriend]}</li>`;
+  ).innerHTML = `<li style="background-color: #fff9eb; color: gold"><span style="color: blue">Tu amigo secreto es: </span>${friends[pickFriend]}🥳</li>
+`;
+};
+
+const showResults = () => {
+  const colors = ["#FF5733", "#33FF57", "#3357FF", "#F933FF", "#33FFF5"];
+  let currentIndex = 0;
+
+  //function to change the background color and update the <li>
+  const updateStyle = () => {
+    const ulElement = document.getElementById("result");
+
+    if (ulElement) {
+      //Change background color
+      ulElement.style.backgroundColor = colors[currentIndex % colors.length];
+
+      //Update the content of the <ul> with a new <li>
+      ulElement.innerHTML = `<li>${
+        friends[currentIndex % friends.length]
+      }</li>`;
+
+      //Advance to the following index
+      currentIndex++;
+    }
+  };
+
+  //Change the color and li Every  0.5 seconds
+  const intervalId = setInterval(updateStyle, 500);
+
+  //Stop the change of colors and execute another function after 5 seconds
+  setTimeout(() => {
+    clearInterval(intervalId); //Stop the change of colors
+    pickRandomFriend(); //Select the name
+  }, 5000); //5 seconds of time in milliseconds
 };
